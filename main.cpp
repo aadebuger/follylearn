2b07832d7df1f3bd05417269762f29210c1ca53e
hua zhuang <zh@zqf.com.cn>
2017-01-16 16:54:50 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <folly/FileUtil.h>
+#include <folly/String.h>
+using namespace folly;
+int main( void )
+{
+    folly::fbstring str = "Hello World";
+    std::cout << str << std::endl;
+    
+}
